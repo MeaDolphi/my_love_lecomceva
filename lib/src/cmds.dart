4b694c86fb8eb361ba756d713e176358fc220d44
MeaDolphi <60520039+MeaDolphi@users.noreply.github.com>
2022-09-16 23:11:29 +0300

    update cmds.dart

--- lib/src/cmds.dart
+++ lib/src/cmds.dart
@@ -1,9 +1,10 @@
 Map<String, String> _commands = {
+  default: 'Я потеряла твою команду, родной',
   'start': 'Привет, Серёжа!',
 };
 
 class BotCommands {
   String? getText(String command) {
-    return _commands[command!];
+    return _commands[command];
   }
 }
