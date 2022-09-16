e575ce5e99a83d23dfdba4e2ee792eae75f6647f
MeaDolphi <60520039+MeaDolphi@users.noreply.github.com>
2022-09-16 22:49:21 +0300

    update dart_project.dart

--- lib/dart_project.dart
+++ lib/dart_project.dart
@@ -4,6 +4,7 @@ import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
 
 void main() async {
+  var botCommands = BotCommands();
   var BOT_TOKEN = '5603372057:AAE5-jo98CVGWCmnt3F0IMOA1JW7IjNrcVY';
   final username = (await Telegram(BOT_TOKEN).getMe()).username;
   var teledart = TeleDart(BOT_TOKEN, Event(username!));
@@ -11,5 +12,5 @@ void main() async {
   teledart.start();
   
   teledart.onCommand('start')
-    .listen((message) => message.reply('to Ukraine!'));
+    .listen((message) => message.reply(botCommands.getText(message.keyword)));
 }
