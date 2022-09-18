8a24620739353ce8e9be0819ff09fbefbd852c64
MeaDolphi <60520039+MeaDolphi@users.noreply.github.com>
2022-09-18 08:04:50 +0300

    update cmds.dart

--- lib/src/cmds.dart
+++ lib/src/cmds.dart
@@ -1,7 +1,22 @@
-Map<String?, String> _commands = {
-  'start': 'Привет, Серёжа! Ты правда думаешь, что я тебе смогу помочь? Мне на тебя не все равно. Я не хотела делать тебе больно...',
-  null: 'Я потеряла твою команду, родной...',
+Map<String, List> _commands = {
+  'start': [
+    BotCommandMe((args) {
+      args[0].reply('Привет, Серёжа! Ты правда думаешь, что я тебе смогу помочь? Мне на тебя не все равно. Я не хотела делать тебе больно...');
+    }),
+   ]
 };
+
+class BotCommandMe {
+  final func;
+  
+  BotCommand(func) {
+    this.func = func;
+  };
+  
+  void execute({List args}) {
+    func(args ?? []);
+  };
+}
 
 class BotCommands {
   String getText(String? command) {
@@ -9,7 +24,13 @@ class BotCommands {
     return _commands[command?.substring(1)]!;
   }
   
-  BotCommands(String cmd, ) {
-    
+  BotCommands(Message msg, String cmd) {
+    if (_commands[cmd] != null) {
+      for (int i = 0; i < _commands[cmd].length; i++) {
+        _commands[cmd][i].execute(args: [msg]);
+      }
+    } else {
+      msg.reply('Серёжа... Такой команды нет в твоём коде...');
+    }
   }
 }
